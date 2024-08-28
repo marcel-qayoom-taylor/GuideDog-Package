71a5b3911003c70c6281cf73a9b6f2621f083da9
Marcel Qayoom-Taylor <marcelqayoomtaylor@gmail.com>
2024-08-28 12:00:07 +1000

    fix everything

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,1 @@
-// .js instead of .ts is intentional see this article: https://www.totaltypescript.com/relative-import-paths-need-explicit-file-extensions-in-ecmascript-imports
-
-export { add } from './utils.js';
+export * from './utils';
